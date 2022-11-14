4c93c7a0cb42eec3f367845f1774bbe4aeee9ec9
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2022-11-14 09:49:45 +0000

    getting started step 8

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from src.demo_landing_to_raw import landing_to_raw
 from yetl.flow import Timeslice
 import json
 
-timeslice = Timeslice(2021, 1, 1)
+# timeslice = Timeslice(2021, 1, 1)
+timeslice = Timeslice(2021, 1, 2)
 
 table = "customer_details"
 results = landing_to_raw(table=table, timeslice=timeslice)
