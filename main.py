b7d3b673aa64f700d18d828a9b12e28b2df1c7fc
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2022-12-24 11:34:38 +0000

    getting-started-step-10

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from src.demo_landing_to_raw import landing_to_raw
-from yetl.flow import Timeslice
+from yetl.flow import Timeslice, OverwriteSave
 import json
 
 # timeslice = Timeslice(year=2021, month=1, day=1)
-timeslice = Timeslice(year=2021, month=1, day=2)
+# timeslice = Timeslice(year=2021, month=1, day=2)
+timeslice = Timeslice(year="*", month="*", day="*")
 
 table = "customer_details"
-results = landing_to_raw(table=table, timeslice=timeslice)
+results = landing_to_raw(table=table, timeslice=timeslice, save=OverwriteSave)
 results = json.dumps(results, indent=4, default=str)
 print(results)
 
 table = "customer_preferences"
-results = landing_to_raw(table=table, timeslice=timeslice)
+results = landing_to_raw(table=table, timeslice=timeslice, save=OverwriteSave)
 results = json.dumps(results, indent=4, default=str)
 print(results)
